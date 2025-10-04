e11680a44b003b22aa8cc77693b79a41498abce0
milita <milit@192.168.1.9>
2025-10-04 22:47:48 +0530

    Buffer

---
+++ Interview250Questions/src/tasks_Programmes/StringBuffer.java
@@ -0,0 +1,11 @@
+package tasks_Programmes;
+
+public class StringBuffer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hllo");
+
+	}
+
+}
